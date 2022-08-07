088c19ebdc1f5613c57c04b20e1b84f26ac7c302
sw1128 <65203923+sw1128@users.noreply.github.com>
2022-08-07 12:21:00 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace MovieToGif
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.infile = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.outfile = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.frame = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cut = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 12F);
+            this.button1.Location = new System.Drawing.Point(30, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // infile
+            // 
+            this.infile.Font = new System.Drawing.Font("宋体", 10F);
+            this.infile.Location = new System.Drawing.Point(153, 114);
+            this.infile.Name = "infile";
+            this.infile.Size = new System.Drawing.Size(609, 77);
+            this.infile.TabIndex = 1;
+            this.infile.Text = "未选择";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 10F);
+            this.label2.Location = new System.Drawing.Point(25, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "文件路径：";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 12F);
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(545, 416);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 53);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "开始转换";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.outfile);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.frame);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cut);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.start);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 12F);
+            this.groupBox1.Location = new System.Drawing.Point(30, 177);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(732, 233);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "选项";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("宋体", 12F);
+            this.label12.Location = new System.Drawing.Point(178, 53);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 28);
+            this.label12.TabIndex = 9;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("宋体", 12F);
+            this.label11.Location = new System.Drawing.Point(23, 53);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(110, 28);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "总时长:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10F);
+            this.label1.Location = new System.Drawing.Point(312, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "秒";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.Location = new System.Drawing.Point(647, 186);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 28);
+            this.label8.TabIndex = 12;
+            this.label8.Text = ".gif";
+            // 
+            // outfile
+            // 
+            this.outfile.Location = new System.Drawing.Point(509, 175);
+            this.outfile.Name = "outfile";
+            this.outfile.Size = new System.Drawing.Size(135, 39);
+            this.outfile.TabIndex = 11;
+            this.outfile.Text = "demo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(379, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 28);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "文件名:";
+            // 
+            // frame
+            // 
+            this.frame.Location = new System.Drawing.Point(509, 111);
+            this.frame.Name = "frame";
+            this.frame.Size = new System.Drawing.Size(135, 39);
+            this.frame.TabIndex = 9;
+            this.frame.Text = "24";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(379, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 28);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "帧数/秒:";
+            // 
+            // cut
+            // 
+            this.cut.Location = new System.Drawing.Point(181, 174);
+            this.cut.Name = "cut";
+            this.cut.Size = new System.Drawing.Size(125, 39);
+            this.cut.TabIndex = 7;
+            this.cut.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(23, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 28);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "截取时间:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(23, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 28);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "开始时间:";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(181, 110);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(125, 39);
+            this.start.TabIndex = 0;
+            this.start.Text = "00:00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 15F);
+            this.label6.Location = new System.Drawing.Point(396, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(366, 46);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Made By ERROR 404";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("宋体", 12F);
+            this.button3.Location = new System.Drawing.Point(187, 429);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 40);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "选择";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("宋体", 12F);
+            this.label9.Location = new System.Drawing.Point(24, 435);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(138, 28);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "导出路径:";
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("宋体", 10F);
+            this.label10.Location = new System.Drawing.Point(25, 483);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(737, 53);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "未指定";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(796, 547);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.infile);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mp4 To Gif";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label infile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox frame;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox cut;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox start;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox outfile;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using WMPLib;
+
+namespace MovieToGif
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string command = "";
+        string filePath = "";
+        string startTime = "";
+        string cutTime = "";
+        string frames = "";
+        string fileName = "";
+        string outputPath = "";
+
+        public Double Duration(String file)
+        {
+            WindowsMediaPlayer wmp = new WindowsMediaPlayerClass();
+            IWMPMedia mediainfo = wmp.newMedia(file);
+            return mediainfo.duration;
+        }
+        private string sec_to_hms(double duration)
+        {
+            TimeSpan ts = new TimeSpan(0, 0, Convert.ToInt32(duration));
+            string str = "";
+            if (ts.Hours > 0)
+            {
+                str = "不支持一小时以上的视频";
+            }
+            if (ts.Hours == 0 && ts.Minutes > 0)
+            {
+                str = String.Format("{0:00}", ts.Minutes) + ":" + String.Format("{0:00}", ts.Seconds);
+            }
+            if (ts.Hours == 0 && ts.Minutes == 0)
+            {
+                str = "00:" + String.Format("{0:00}", ts.Seconds);
+            }
+            return str;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog file = new OpenFileDialog();
+            file.Title = "选择一个mp4文件";
+            file.Filter = "视频文件|*.mp4";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                filePath = file.FileName.Replace("\\","/");
+                infile.Text = filePath;
+                double totalTime = Duration(filePath);
+                cut.Text = totalTime.ToString();
+                string date = sec_to_hms(totalTime);
+                label12.Text = date;
+            }
+        }
+
+        private string getCommand()
+        {
+            startTime = start.Text;
+            cutTime = cut.Text;
+            filePath = infile.Text;
+            frames = frame.Text;
+            command = "ffmpeg -ss " + startTime + " -t " + cutTime + " -i " + filePath + " -r " + frames + " " + outputPath;
+            return command;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (infile.Text == "未选择")
+            {
+                MessageBox.Show("请选择一个文件","提示");
+                return;
+            }
+            if (label10.Text == "")
+            {
+                MessageBox.Show("请选择导出路径", "提示");
+                return;
+            }
+            else
+            {
+                command = getCommand();
+                Process p = new Process();
+                p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.UseShellExecute = false;    //是否使用操作系统shell启动
+                p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+                p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                p.StartInfo.CreateNoWindow = true;
+                p.Start();
+                p.StandardInput.WriteLine(command);
+                System.Threading.Thread.Sleep(1000);
+                p.StandardInput.WriteLine("exit");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                fileName = folder.SelectedPath.Replace("\\", "/");
+                outputPath = fileName + "/" + outfile.Text + ".gif";
+                label10.Text = outputPath;
+            }
+        }
+    }
+}
